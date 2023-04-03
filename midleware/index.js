5dd11ff205aa8387bef60cd2e98c51466837de6e
rahmat-hidayat24 <lsdayat@gmail.com>
2023-04-04 00:00:41 +0700

    change middleware

--- midleware/index.js
+++ midleware/index.js
@@ -2,7 +2,13 @@ const { AdminModel } = require("../src/models");
 const { hashPassword } = require("../utils");
 
 const Middleware = ({ app, bodyParser, cors, express }) => {
-  app.use(cors());
+  // app.use(cors());
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE, PUT, OPTIONS");
+    next();
+  });
   app.options("*", cors());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
